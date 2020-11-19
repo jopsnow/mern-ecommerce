26acecd63325758473af6c463962fcd7c0d2ffbb
Jop Bard <2424095-KoshkaP@users.noreply.gitlab.com>
2020-11-19 19:15:56 +0800

    Setup header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+
+// Components 
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Apitong Bike Shop</h1>
+    <Header/>
+    <main className="py-3">
+    <Container>
+      <h1>Welcome to Bike Shop</h1>
+    </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
