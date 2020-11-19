01a5565b2c22a97b2fbf5ff1a438d367d9dda1a9
Jop Bard <2424095-KoshkaP@users.noreply.gitlab.com>
2020-11-19 19:49:01 +0800

    Add homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 // Components 
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
     <Header/>
     <main className="py-3">
     <Container>
-      <h1>Welcome to Bike Shop</h1>
+      <HomeScreen />
     </Container>
     </main>
     <Footer/>
